c37abb52a4bf8990977b50b82ba73340dbf27e38
Marten Truu <marten.truu@gmail.com>
2017-04-09 21:35:32 +0300

    😻 add cats

---
+++ spinner/Card.js
@@ -0,0 +1,22 @@
+
+function Card({ imageSrc, headerText, textContent }) {
+  const card = document.createElement('div')
+  card.className = 'card'
+
+  const image = card.appendChild(document.createElement('img'));
+  image.className = 'card-img-top img-fluid'
+  image.src = imageSrc;
+
+  const content = card.appendChild(document.createElement('div'))
+  content.className = 'card-block'
+
+  const header = content.appendChild(document.createElement('h4'))
+  header.className = 'card-title'
+  header.textContent = headerText
+
+  const text = content.appendChild(document.createElement('p'))
+  text.className = 'card-text'
+  text.textContent = textContent
+
+  return card
+}
--- spinner/spinner.js
+++ spinner/spinner.js
@@ -1,30 +1,28 @@
-const PRIZE_COUNT = 120;
+const PRIZE_COUNT = 120
 
 function placePrizes(row) {
-  for (var i = 0; i < PRIZE_COUNT; i ++) {
-    var cardContainer = document.createElement('div');
-    cardContainer.className = 'card-container p-2 col-4';
+  for (let i = 0; i < PRIZE_COUNT; i ++) {
+    const cardContainer = document.createElement('div')
+    cardContainer.className = 'card-container p-2 col-4'
 
-    const card = document.createElement('div')
-    card.className = 'card p-2'
+    const card = Card({
+      imageSrc: 'http://lorempixel.com/400/400/cats/',
+      headerText: i,
+      textContent: i,
+    })
     cardContainer.appendChild(card)
-    var header = card.appendChild(document.createElement('h4'));
-    var content = card.appendChild(document.createElement('p'));
-    header.textContent = i;
-    content.textContent = 'bollocks';
-
-    row.appendChild(cardContainer);
+    row.appendChild(cardContainer)
   }
-
 }
 
 function spin() {
   const winningCard = Math.round(Math.random() * PRIZE_COUNT)
+  console.log(winningCard)
   document.querySelector('.spinner').style.transform = `translateX(${-(winningCard - 1) * 33.3333}%)`
 }
 
 function init() {
-  placePrizes(document.querySelector('.spinner'));
+  placePrizes(document.querySelector('.spinner'))
 }
 
-window.addEventListener('load', init, false);
+window.addEventListener('load', init, false)
