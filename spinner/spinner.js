90506256b269903c201c4b4b33c718c8ef7d26f9
Marten Truu <marten.truu@gmail.com>
2017-04-11 23:49:57 +0300

    🐄🙀 add more cats, also, kill more cats

--- spinner/spinner.js
+++ spinner/spinner.js
@@ -32,12 +32,14 @@ function spin() {
     })
   document.querySelector('.spinner').style.transform = `translateX(${-(winningCard - 1) * 33.3333}%)`
   spinButton.classList.add('disabled')
+  spinButton.disabled = true
   setTimeout(incrementLives, 9250)
 }
 
 function incrementLives() {
   saveCounter.innerHTML = ++totalSaves
   spinButton.classList.remove('disabled')
+  spinButton.disabled = false
 }
 
 function init() {
